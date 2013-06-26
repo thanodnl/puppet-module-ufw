b49f13901e03b4564d94fbd8f2fe770c1a35185f
Nils Dijk <me@thanod.nl>
2013-06-26 11:25:31 +0200

    Check log level before applying

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -4,5 +4,6 @@ define ufw::logging($level='low') {
     command => "ufw logging $level",
     require => Exec['ufw-default-deny'],
     before  => Exec['ufw-enable'],
+    unless  => "sudo ufw status verbose | grep \"Logging: $level\"",
   }
 }
